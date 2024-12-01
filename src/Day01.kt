a4d588c0a6379df2a697ebaeb06b4398a01ed39b
Sipos Dávid <sipisipos@gmail.com>
2024-12-01 21:49:04 +0100

    Solve Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,68 @@
+import kotlin.math.absoluteValue
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun read(input: List<String>): Pair<IntArray, IntArray> {
+        val groupLeft = IntArray(input.size)
+        val groupRight = IntArray(input.size)
+
+        input.forEachIndexed { index, line ->
+            val items = line.split("   ").map { it.toInt() }
+            if (items.size != 2) {
+                throw IllegalArgumentException()
+            }
+            groupLeft[index] = items[0]
+            groupRight[index] = items[1]
+        }
+
+        return Pair(groupLeft, groupRight)
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+
+    fun part1(input: List<String>): Long {
+        val (groupLeft, groupRight) = read(input)
+
+        groupLeft.sort()
+        groupRight.sort()
+
+        var distance = 0L
+
+        groupLeft.forEachIndexed { index, item ->
+            distance += (item - groupRight[index]).absoluteValue
+        }
+
+        return distance
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+    fun part2(input: List<String>): Long {
+        val (groupLeft, groupRight) = read(input)
 
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+        val occurrences = mutableMapOf<Int, Int>()
 
-    // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+        groupRight.forEach { occurrences[it] = 0 }
+
+        groupLeft.forEach {
+            if (occurrences.containsKey(it)) {
+                occurrences[it] = occurrences[it]!! + 1
+            }
+        }
+
+        return groupRight.sumOf {
+            val occurrence = occurrences[it]
+            if (occurrence != null) {
+                (occurrence * it).toLong()
+            } else {
+                0L
+            }
+        }
+    }
+
+    // Solutions
+    val solutions = readInput("Day01_solutions").map { line ->
+        val items = line.split("   ").map { it.toInt() }
+        Pair(items[0].toLong(), items[1].toLong())
+    }
+
+    // Run
+    result("Test", "Day01_test", solutions[0], ::part1, ::part2)
+    result("Actual", "Day01", solutions[1], ::part1, ::part2)
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -19,3 +19,26 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+/**
+ * Pretty print a result for a given input file
+ */
+fun <T1, T2> result(
+    name: String,
+    path: String,
+    solutions: Pair<T1, T2>,
+    part1: (List<String>) -> T1,
+    part2: (List<String>) -> T2,
+) {
+    name.println()
+    val test = readInput(path)
+    partResult(1, solutions.first, part1(test))
+    partResult(2, solutions.second, part2(test))
+}
+
+/**
+ * Pretty print a part's result
+ */
+private fun <T> partResult(part: Int, expected: T, actual: T) {
+    "$part: expected ${expected}, got ${actual}. ${if (expected == actual) "✅" else "❌"}".println()
+}
